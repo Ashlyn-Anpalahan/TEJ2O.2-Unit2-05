d2550b16166e6c987856548903b5e70c84e36f87
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-02-24 15:35:28 -0500

    Changed code to kelvin

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+*
+* Created by: Ashlyn
+* Created on: Feb 2026
+* This program will show the current temperature in Kelvin
 */
 
-basic.showString('Hello, World!')
+let temperatureInKelvin: number
+let roundedKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    let temperature = input.temperature()
+    temperatureInKelvin = (temperature + 273.15)
+    roundedKelvin = Math.round(temperatureInKelvin)
+
+    basic.clearScreen()
+    basic.showString("The temperature is: " + (roundedKelvin) + " K.")
+})
